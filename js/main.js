6d86787be7c8ea505045d16ae78007f9a1a5f269
Micah Borghese <micahborghese@gmail.com>
2022-07-22 12:59:53 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,18 +10,14 @@ window.onorientationchange = function() { document.body.scrollTop = 0; }
 var typed = new Typed('#typed', {
 
 	strings: [
-    'College Student at KU.',
-    'Aspiring Software Developer.',
-    'Life Long Learner.',
-    'Check Out My Socials!'
-    ],
+	'College Student at KU.',
+	'Aspiring Software Developer.',
+	'Life Long Learner.',
+	'Check Out My Socials!'
+	],
 
 	typeSpeed: 60,
 	startDelay: 1250,
 	backSpeed: 50,
 	loop: true
-});
-
-document.querySelector(".banner-close").addEventListener("click", function() {
-	this.closest(".banner").style.display = "none";
 });
